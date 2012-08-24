57557a23234d48e66c7a65df3458d528b7a0e1cb
Kamule <benjamin.herbomez@esial.net>
2012-08-24 11:55:31 +0300

    Update src/daemon.cpp
    Maj du displayUsage

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -80,9 +80,13 @@ void Daemon::handleCommand(const QString& message){
 
 
 void Daemon::displayUsage(){
-    std::cerr << "usage : ewo-app [action]" << std::endl;
-    std::cerr << "  action :" << std::endl;
-    std::cerr << "    --stop   : stop the daemon " << std::endl;
-    std::cerr << "    --reload : recharge les application " << std::endl;
-    std::cerr << "    --state  : donne l'état du daemon' " << std::endl;
+    std::cerr << "usage : ewo-app [action] [params]" << std::endl
+              << "  action :" << std::endl
+              << "    start  : start the daemon " << std::endl
+              << "    stop   : stop the daemon " << std::endl
+              << "    reload : reload applications " << std::endl
+              << "    state  : give state of applications on the daemon " << std::endl
+              << "      option --json send result in json format" << std::endl
+              << "  params : list of applications" << std::endl
+              << "           if there is no param all applications are impacted by the action" << std::endl;
 }
